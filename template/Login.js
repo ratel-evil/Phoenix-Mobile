cdae9dba930fc4afb0e63dd8bc11401f036db232
Adriano-Matias <adriano-matias-souza@hotmail.com>
2020-08-27 21:27:53 -0300

    Iniciando a tela de Cadastro

--- template/Login.js
+++ template/Login.js
@@ -1,99 +1,107 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import {
-    View,
-    Image,
-    Button,
-    TextInput,
-    StyleSheet,
-    KeyboardAvoidingView,
-    SafeAreaView,
-    TouchableWithoutFeedback,
-    Keyboard,
-    Platform,
-    Dimensions,
-    Text
+  View,
+  Image,
+  Button,
+  TextInput,
+  StyleSheet,
+  KeyboardAvoidingView,
+  SafeAreaView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Platform,
+  Dimensions,
+  Text,
 } from 'react-native';
-import LogoPhoenix from '../assets/logo_phoenix/Phoenix-03.png'
-import BackgroundImage from '../assets/fundo01-01.png'
+import LogoPhoenix from '../assets/logo_phoenix/Phoenix-03.png';
+import BackgroundImage from '../assets/fundo01-01.png';
 
+function Login({navigation}) {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
 
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : null}
+      style={styles.container}>
+      <Image source={LogoPhoenix} style={styles.logo} />
+      <TextInput
+        style={styles.input}
+        placeholder={'Usuário'}
+        onChangeText={setUsername}
+      />
 
-function Login({ navigation }) {
-    
-    const [username, setUsername] = React.useState('');
-    const [password, setPassword] = React.useState('');
-   
+      <TextInput
+        style={styles.input}
+        placeholder={'Senha'}
+        onChangeText={setPassword}
+        secureTextEntry={true}
+      />
 
-    return (
-        <KeyboardAvoidingView 
-            behavior={Platform.OS === "ios" ? "padding" : null} 
-            style={styles.container} 
-            >
-            <Image source={LogoPhoenix} style={styles.logo } />
-            <TextInput style={styles.input} placeholder={'Usuário'} onChangeText={setUsername}></TextInput>
-            
-            <TextInput style={styles.input} placeholder={'Senha'} onChangeText={setPassword} secureTextEntry={true}></TextInput>
-            
-            <View style={styles.buttons}>
-                <Button color="#63b370" title={'Cadastrar'} onPress={() => {
-                    navigation.navigate('SignUp')
-
-                }} />
-                <Button color="#63b370" title={'Entrar'} onPress={() => console.warn('login')} />
-            </View>
-            <Image source={BackgroundImage} style={styles.backImage} />
-        </KeyboardAvoidingView>
-    );
+      <View style={styles.buttons}>
+        <Button
+          color="#63b370"
+          title={'Cadastrar'}
+          onPress={() => {
+            navigation.navigate('SignUp');
+          }}
+        />
+        <Button
+          color="#63b370"
+          title={'Entrar'}
+          onPress={() => console.warn('login')}
+        />
+      </View>
+      <Image source={BackgroundImage} style={styles.backImage} />
+    </KeyboardAvoidingView>
+  );
 }
 const sizes = Dimensions.get('window');
 const styles = StyleSheet.create({
-    container: {
-        width: sizes.width,
-        height: sizes.height -50,
-        justifyContent: 'space-evenly',
-        alignItems: 'center',
-        backgroundColor: "#efefef"
+  container: {
+    width: sizes.width,
+    height: sizes.height - 50,
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    backgroundColor: '#efefef',
+  },
+  logo: {
+    resizeMode: 'contain',
+    height: sizes.width / 2,
+  },
+  input: {
+    borderColor: 'grey',
+    borderWidth: 1,
+    width: 300,
+    height: 45,
+    paddingTop: 10,
+    borderRadius: 5,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 5,
     },
-    logo: {
-        resizeMode: 'contain',
-        height: sizes.width /2 ,
-    },
-    input: {
-        borderColor: 'grey',
-        borderWidth: 1,
-        width: 300,
-        height: 45,
-        paddingTop: 10,
-        borderRadius: 5,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 5,
-        },
-        shadowOpacity: 0.36,
-        shadowRadius: 6.68,
-        elevation: 11,
-        backgroundColor: '#fff'
-    },
-   
-    buttons: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        width: '52%',
-        height: 20,
+    shadowOpacity: 0.36,
+    shadowRadius: 6.68,
+    elevation: 11,
+    backgroundColor: '#fff',
+  },
 
-    },
-    backImage: {
-        position: 'absolute',
-        width: "200%",
-        height: 300,
-        opacity: 0.2,
-        bottom: 0,
-        zIndex: -1
-    }
-})
-
-
+  buttons: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '52%',
+    height: 20,
+  },
+  backImage: {
+    position: 'absolute',
+    width: '200%',
+    height: 300,
+    opacity: 0.2,
+    bottom: 0,
+    zIndex: -1,
+  },
+});
 
 export default Login;
--- template/SignUp.js
+++ template/SignUp.js
@@ -1,13 +1,99 @@
-import React from 'react';
-import { View, KeyboardAvoidingView, Text } from 'react-native';
+import React, {useRef} from 'react';
+import {
+  Button,
+  Dimensions,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+} from 'react-native';
+import {Form} from '@unform/mobile';
+import Input from '../components/Input';
+
+import LogoPhoenix from '../assets/logo_phoenix/Phoenix-03.png';
 
 const SignUp = ({navigation}) => {
-    return ( 
-            <KeyboardAvoidingView>
-                <Text> Cadastre-se</Text>
-            </KeyboardAvoidingView>
+  const formRef = useRef(null);
+  function handleSubmit(data) {
+    console.warn('Cadastrar');
+  }
 
-     );
-}
- 
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : null}
+      style={styles.container}>
+      <Image source={LogoPhoenix} style={styles.logo} />
+      <Form ref={formRef} onSubmit={handleSubmit}>
+        <Input
+          style={styles.input}
+          name="nome"
+          type="text"
+          placeholder="Nome"
+        />
+        <Input
+          style={styles.input}
+          name="sobrenome"
+          type="text"
+          placeholder="Sobrenome"
+        />
+        <Input
+          style={styles.input}
+          name="email"
+          type="email"
+          placeholder="Email"
+        />
+        <Input
+          style={styles.input}
+          name="password"
+          type="password"
+          placeholder="Senha"
+        />
+        <Input
+          style={styles.input}
+          name="password"
+          type="password"
+          placeholder="Confirmar Senha"
+        />
+        <Button
+          color="#63b370"
+          title="Cadastrar"
+          onPress={() => formRef.current.submitForm()}
+        />
+      </Form>
+    </KeyboardAvoidingView>
+  );
+};
+
+const sizes = Dimensions.get('window');
+const styles = StyleSheet.create({
+  container: {
+    width: sizes.width,
+    height: sizes.height - 50,
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    backgroundColor: '#efefef',
+  },
+  logo: {
+    resizeMode: 'contain',
+    height: sizes.width / 2,
+  },
+  input: {
+    borderColor: 'grey',
+    borderWidth: 1,
+    width: 300,
+    height: 45,
+    paddingTop: 10,
+    borderRadius: 5,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.36,
+    shadowRadius: 6.68,
+    elevation: 11,
+    backgroundColor: '#fff',
+  },
+});
+
 export default SignUp;
